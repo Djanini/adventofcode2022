961070532249ce03cca6ce1d9f364d5a5c071cac
Djanini Markovic <djanini.m@gmail.com>
2022-12-03 18:06:56 +0100

    Update for day1

--- src/day1.jl
+++ src/day1.jl
@@ -1,12 +1,13 @@
 using BenchmarkTools
 
-function solve_day1()
+function solve()
     input_file = open("./data/day1.txt") do file
         read(file, String)
     end
-
-    totals = partialsort([sum(parse.(Int, split(elf))) for elf in split(input_file, "\n\n")], 1:3, rev=true)
+    totals = [sum(parse.(Int, split(elf))) for elf in split(input_file, "\n\n")]
+    partialsort!(totals, 1:3, rev=true)
     return totals[1], sum(totals[1:3])
 end
 
-println(@btime solve_day1())
+println(solve())
+@btime solve()
