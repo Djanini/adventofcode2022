b7ed3e4d2f0b681e81e09237ec1e575736277d86
Djanini Markovic <djanini.m@gmail.com>
2022-12-04 11:57:59 +0100

    Simplified functions day 4

--- src/day4.jl
+++ src/day4.jl
@@ -1,17 +1,8 @@
 using BenchmarkTools
 
-function parse_pair(pair)
-    return [range(parse.(Int, task)...) for task in split.(pair, "-")]
-end
-
-
-function pair_fully_contains(pair::Vector{UnitRange{Int64}})
-    return pair[1] ⊆ pair[2] || pair[2] ⊆ pair[1]
-end
-
-function pair_overlaps(pair::Vector{UnitRange{Int64}})
-    return length(pair[1] ∩ pair[2]) != 0
-end
+parse_pair(pair::Vector{SubString{String}}) = [range(parse.(Int, task)...) for task in split.(pair, "-")]
+pair_fully_contains(pair::Vector{UnitRange{Int64}}) = return pair[1] ⊆ pair[2] || pair[2] ⊆ pair[1]
+pair_overlaps(pair::Vector{UnitRange{Int64}}) = length(pair[1] ∩ pair[2]) != 0
 
 function solve()
     input_file = open("./data/day4.txt") do file
