88b60c8925299f7a91b75c802e456f33f75d74ce
Djanini Markovic <djanini.m@gmail.com>
2022-12-07 15:50:45 +0100

    Added day 6

--- src/day6.jl
+++ src/day6.jl
@@ -1,10 +1,27 @@
 using BenchmarkTools
 
 function solve()
-    input_file = open("./data/day6.txt") do file
+    input_string::String = open("./data/day6.txt") do file
         read(file, String) |> rstrip
     end
-    return input_file
+
+    start_of_packet::Int = 0
+    start_of_message::Int = 0
+
+    for i in 1:length(input_string)-3
+        if length(Set(input_string[i:i+3])) == 4
+            start_of_packet = i + 3
+            break
+        end
+    end
+    for i in 1:length(input_string)-13
+        if length(Set(input_string[i:i+13])) == 14
+            start_of_message = i + 13
+            break
+        end
+    end
+
+    return start_of_packet, start_of_message
 end
 
 println(solve())
