db978c6147aa414291f577348dd69f2510daea70
Djanini Markovic <djanini.m@gmail.com>
2022-12-07 15:35:19 +0100

    Added day 7

--- src/day7.jl
+++ src/day7.jl
@@ -1,11 +1,33 @@
 using BenchmarkTools
 
 function solve()
-    input_file = open("./data/day7.txt") do file
+    input_string = open("./data/day7.txt") do file
         read(file, String) |> rstrip
     end
-    return input_file
+    input_lines::Vector{Vector{String}} = replace(input_string,
+        "\$ cd .." => "dir_up,",
+        r"\$ cd (\w+?)" => s"cd_to,\1",
+        r"(\d+?) .*" => s"add_size,\1",
+        r"((dir \w+)|(\$ ls)|(\$ cd /))\n" => "") |>
+                                          lines -> split(lines, "\n") .|>
+                                                   line -> split(line, ',')
+
+    cmd_functions = Dict([
+        "add_size" => (paths, dirsizes, size) -> mergewith!(+, dirsizes, Dict(path => parse(Int, size) for path ∈ paths)),
+        "cd_to" => (paths, dirsizes, dir) -> push!(paths, paths[end] * dir),
+        "dir_up" => (paths, dirsizes, _) -> pop!(paths)
+    ])
+
+    paths::Vector{String} = ["/"]
+    dirsizes = Dict{String,Int}()
+
+    for (cmd, arg) ∈ input_lines
+        cmd_functions[cmd](paths, dirsizes, arg)
+    end
+
+    sizes_arr::Vector{Int} = collect(values(dirsizes))
+    return sum(filter(s -> s ≤ 100_000, sizes_arr)), minimum(filter(s -> s ≥ dirsizes["/"] - 40_000_000, sizes_arr))
 end
 
-println(solve())
+solve()
 @btime solve()
